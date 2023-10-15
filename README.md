317c35bc2004bbfc8539bbec1045717524c41835
dukaitamas <66377339+dukaitamas@users.noreply.github.com>
2023-10-15 19:48:21 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Anakin-website
-trying, not perfect
+# My first website222
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/tamas-dukai/pen/KKdOjPa](https://codepen.io/tamas-dukai/pen/KKdOjPa).
+
---
+++ script.js
@@ -0,0 +1,6 @@
+  function playSound(chewy) {
+  document.getElementById(chewy).play();
+};
+/*  <a href="javascript:playSound('pod')" class="riot">Experience</a>   +++plays sound on click     */
+
+/* <a href="#"onmouseenter="playSound('r2d2')" class="riot">Bio</a>  +++plays sound on hover*/
