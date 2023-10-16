e94100138b2e6b26d6531d2139fcf043d1f68f75
dukaitamas <66377339+dukaitamas@users.noreply.github.com>
2023-10-16 21:37:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,17 @@
 
 
 function playSound("r2d2") {
-  document.getElementById("r2d2").play();
+  document.getElementById("r2d2").play("r2d2");
 };
 
 function playSound("saberon") {
-  document.getElementById("saberon").play();
+  document.getElementById("saberon").play("saberon");
 };
 
 function playSound("pod") {
-  document.getElementById("pod").play();
+  document.getElementById("pod").play("pod");
 };
 
 function playSound("laser") {
-  document.getElementById("laser").play();
+  document.getElementById("laser").play("laser");
 };
