f550530b3ba16c75645c888b4170ce4aec69ff84
dukaitamas <66377339+dukaitamas@users.noreply.github.com>
2023-10-16 21:08:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-  function playSound(chewy) {
-  document.getElementById(chewy).play();
-};
+
 /*  <a href="javascript:playSound('pod')" class="riot">Experience</a>   +++plays sound on click     */
 
 /* <a href="#"onmouseenter="playSound('r2d2')" class="riot">Bio</a>  +++plays sound on hover*/
+
+
+function playSound('r2d2') {
+  document.getElementById('r2d2').play();
+};
+
+function playSound('saberon') {
+  document.getElementById('saberon').play();
+};
+
+function playSound('pod') {
+  document.getElementById('pod').play();
+};
+
+function playSound('laser') {
+  document.getElementById('laser').play();
+};
